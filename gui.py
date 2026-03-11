c486111419be9d2cec1d3e6454484d829f4ad1d2
Kurogane871 <mkdreadsword@gmail.com>
2026-03-11 11:40:51 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -12,13 +12,6 @@ tk.Label(window, text="Resume Reformatting", font=("Arial", 25)).grid(row=0, col
 form_pane = tk.Frame(window)
 form_pane.grid(row=1, column=0)
 
-#file write
-file = open(".txt", "w")
-file.write("")
-file.close("")
-
-update(.to_string())
-
 # Heading/Title widget
 header = tk.Label(window, text="Resume Reformatting", font=("Arial", 25)).grid(row=0, column=0, columnspan=3)
 
